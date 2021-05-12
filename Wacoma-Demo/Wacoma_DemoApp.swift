4fea032309c3f73c514cedf4f5ce94c839717bac
Jim Hanson <jim@jehanson.org>
2021-05-12 13:15:52 -0400

    Initial Commit

---
+++ Wacoma-Demo/Wacoma_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wacoma_DemoApp.swift
+//  Wacoma-Demo
+//
+//  Created by Jim Hanson on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Wacoma_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
