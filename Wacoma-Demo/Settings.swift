21e10153d2d26aae07f65bde4d73ae8111231512
Jim Hanson <jim@jehanson.org>
2021-11-01 13:05:57 -0400

    tweak

--- Wacoma-Demo/Settings.swift
+++ Wacoma-Demo/Settings.swift
@@ -13,6 +13,7 @@ struct Settings : View {
     @State var group1 = SettingsGroup()
     @State var group2 = SettingsGroup().itemStyle(.narrow)
 
+    @State var foo: String = "Foo"
     @State var alpha: Bool = false
     @State var beta: Int = 0
     @State var gamma: Double = 0
@@ -20,30 +21,34 @@ struct Settings : View {
     @State var epsilon: String = "grooved"
 
     var body: some View {
-        // HStack {
+        VStack(alignment: .leading, spacing: 10) {
             // Spacer()
-            VStack(alignment: .leading, spacing: 10) {
-                // Spacer()
-                // Divider().background(UIConstants.darkGray)
 
+            HStack {
                 groupContent("Group 1 (default)", $group1)
+                Spacer()
                     .border(Color.gray)
-
+            }
+            Divider().background(UIConstants.darkGray)
+            HStack {
                 groupContent("Group 2 (narrow)", $group2)
                     .frame(maxWidth: 320)
-                    .border(Color.gray)
-
                 Spacer()
+                    .border(Color.gray)
             }
-            // Spacer()
-        //}
+            Spacer()
+                .border(Color.gray)
+        }
     }
 
     func groupContent(_ name: String, _ group: Binding<SettingsGroup>) -> some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text(name)
+            ReadOnlySetting("Foo", $foo, group)
             TickyboxSetting("Alpha", $alpha, group, "Yes", "No")
+                .border(Color.blue)
             SteppedSetting("Beta", $beta, group, 0, 100, [1, 10, 50])
+                .border(Color.blue)
             RangeSetting("Gamma", $gamma, group, 0, 100, 1)
             ChoiceSetting("Delta", $delta, group, ["Fee", "Fie", "Foe", "Fum"])
             TextSetting("Epsilon", $epsilon, group)
